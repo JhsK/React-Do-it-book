3be4f2f6bcf27a869a707ecdfade850f23552b1d
JhsK <qwe6293@nate.com>
2021-02-01 21:56:24 +0900

    03-4 컴포넌트 상태 관리하기

---
+++ src/03/PropsComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropsTypes from "prop-types";
+
+class PropsComponent extends React.Component {
+  render() {
+    return <div className="message-container">{this.props.name}</div>;
+  }
+}
+
+PropsComponent.PropsTypes = {
+  name: PropsTypes.string,
+};
+
+export default PropsComponent;
